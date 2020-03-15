b0b1ea4f38cb6cf4a91515bb6e38e13fad8a5531
Сергей <gultyayev.sergey@gmail.com>
2020-03-15 20:01:21 +0200

    123

---
+++ projects/app1/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Lib1Module } from 'lib1';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    Lib1Module
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
